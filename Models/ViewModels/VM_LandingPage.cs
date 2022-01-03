732830e474fda38e1b6c96ecc379b01bc597fabc
mike velasco <mvelasco2001@hotmail.com>
2022-01-03 15:18:55 -0600

    Add landing page viewmodel

---
+++ Models/ViewModels/VM_LandingPage.cs
@@ -0,0 +1,16 @@
+﻿using ReelJunkies.Models.Database;
+using ReelJunkies.Models.TmDb;
+using System.Collections.Generic;
+
+namespace ReelJunkies.Models.ViewModels
+{
+    public class VM_LandingPage
+    {
+        public List<Collection> CustomCollections { get; set; }
+        public MovieSearch NowPlaying { get; set; }
+        public MovieSearch Popular { get; set; }
+        public MovieSearch TopRated { get; set; }
+        public MovieSearch Upcomming { get; set; }
+
+    }
+}
