028373d439cf5d1583e19a7d28f7d040f7592953
Mike Velasco <mvelasco2020@outlook.com>
2022-01-05 22:34:26 -0600

    create interface for database mapping, image service and remote movie service to get data from api

---
+++ Services/Interfaces/IDataMappingService.cs
@@ -0,0 +1,12 @@
+﻿using ReelJunkies.Models.Database;
+using ReelJunkies.Models.TmDb;
+using System.Threading.Tasks;
+
+namespace ReelJunkies.Services.Interfaces
+{
+    public interface IDataMappingService
+    {
+        Task<Movie> MapMovieDetailAsync(MovieDetail movie);
+        ActorDetail MapActorDetail(ActorDetail actor);
+    }
+}
---
+++ Services/Interfaces/IRemoteMovieService.cs
@@ -0,0 +1,14 @@
+﻿using ReelJunkies.Enums;
+using ReelJunkies.Models.TmDb;
+using System.Threading.Tasks;
+
+namespace ReelJunkies.Services.Interfaces
+{
+    public interface IRemoteMovieService
+    {
+        Task<MovieDetail> MovieDetailAsync(int id);
+        Task<MovieSearch> MovieSearchAsync(MovieCategory category, int count);
+
+        Task<ActorDetail> ActorDetail(int id);
+    }
+}
