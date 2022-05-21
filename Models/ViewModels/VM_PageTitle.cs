14c988d1bb804d77b272d5be51a69a90b9598edb
mike velasco <mvelasco2001@hotmail.com>
2022-05-21 18:20:24 -0500

    start coding Account pages

--- Models/ViewModels/VM_PageTitle.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ReelJunkies.Models.ViewModels
-{
-    public class VM_PageTitle
-    {
-        public string FirstString { get; set; }
-        public string SecondString { get; set; }
-    }
-}
