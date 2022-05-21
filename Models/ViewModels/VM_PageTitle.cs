7bb9280710bf5bf627b941b3ca2bd5cd39c86354
mike velasco <mvelasco2001@hotmail.com>
2022-05-21 15:57:54 -0500

    create partial view for header

---
+++ Models/ViewModels/VM_PageTitle.cs
@@ -0,0 +1,8 @@
+﻿namespace ReelJunkies.Models.ViewModels
+{
+    public class VM_PageTitle
+    {
+        public string FirstString { get; set; }
+        public string SecondString { get; set; }
+    }
+}
